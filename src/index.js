c97d8d01f980a9e58c1c0a0ced039b4a9278c91a
zeusfrp-dev <zeus.frp@gmail.com>
2025-12-02 13:14:33 -0300

    Desativado PWA completamente - atualização imediata

--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Limpa o Service Worker em cache para garantir atualizaÃ§Ãµes
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for (let registration of registrations) {
-      registration.unregister();
-    }
-  });
-}
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
