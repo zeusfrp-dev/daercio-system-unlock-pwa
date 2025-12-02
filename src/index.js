5ffc60242ff7aae25cada739f975ab5f963b090a
zeusfrp-dev <zeus.frp@gmail.com>
2025-12-02 13:03:55 -0300

    Adicionado limpeza de Service Worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-﻿import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Limpa o Service Worker em cache para garantir atualizações
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+
 root.render(
   <React.StrictMode>
     <App />
